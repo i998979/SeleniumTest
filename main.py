528cba141b77ef3658bc63d31ec72caf857c659e
i998979 <brianx2x@gmail.com>
2022-04-13 04:59:16 +0800

    Update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless") #無頭模式
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36")
 
 chrome_options.add_argument("--enable-javascript")
