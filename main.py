6ce4c5136b96e1445a088f43e80bd5b7045251a9
i998979 <brianx2x@gmail.com>
2022-04-13 04:02:50 +0800

    Update

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver.get("https://www.facebook.com/TybaltYiumanman")
+driver.get("https://www.facebook.com/SabrinaPingpingNG")
 print(driver.page_source)
