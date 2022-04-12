64b5d1054e1b13a53a32aa457f8a8b0759119bcb
i998979 <brianx2x@gmail.com>
2022-04-13 04:31:08 +0800

    Update

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ chrome_options.add_argument("--headless") #無頭模式
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36")
+
+chrome_options.add_argument("--enable-javascript")
+
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"})
