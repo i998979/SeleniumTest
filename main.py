2f08daba8ab36323c3ca1642fb743c31c9d1d99b
i998979 <brianx2x@gmail.com>
2022-04-13 19:24:54 +0800

    Update

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import os
+
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -19,6 +14,7 @@ chrome_options.add_argument("--enable-javascript")
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
-driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent":"Mozilla/5.0 (Linux; Android 10; SM-G9600) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36"})
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+    "userAgent": "Mozilla/5.0 (Linux; Android 10; SM-G9600) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36"})
 driver.get("https://www.facebook.com/SabrinaPingpingNG/")
 print(driver.page_source)
