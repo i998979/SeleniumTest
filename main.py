78a564655fa6b77f056466b917195e50a4172b31
i998979 <brianx2x@gmail.com>
2022-04-13 19:01:51 +0800

    Update

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64
 chrome_options.add_argument("--enable-javascript")
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-driver.implicitly_wait(15)
+WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "lzcic4wl")))
 
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"})
 driver.get("https://www.facebook.com/SabrinaPingpingNG/")
